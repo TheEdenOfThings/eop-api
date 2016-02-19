2533711058c3c63ab6096cfa1a29b73ab5c14035
Ben Whorwood <info@mube.co.uk>
2016-02-19 12:58:22 +0000

    UNTESTED - initial implementation of sensors API (for logging data).

--- src/sensor.php
+++ src/sensor.php
@@ -1,2 +1,52 @@
 <?php
-    echo 0;
+include 'init.php';
+
+$stationId  = int($_GET['id']);
+$sensorType = trim($_GET['type']);
+
+$value    = int(@$_POST['value']);
+$sequence = int(@$_POST['sequence']);
+$live     = int(@$_POST['live']) == 0 ? false : true;
+
+$sql = <<<EOF
+INSERT INTO sensor_data
+(sensor_id, value, sequence, date_added)
+VALUES
+((
+    SELECT se.id
+    FROM stations s
+
+    INNER JOIN zones z
+    ON s.zone_id = s.id
+
+    INNER JOIN locations l
+    ON z.id = l.zone_id
+
+    INNER JOIN sensors se
+    ON l.id = se.location_id
+
+    INNER JOIN sensor_types st
+    ON se.sensor_type_id = st.id
+
+    WHERE s.id=:stationId AND st.type=:sensorType
+), 
+:value :sequence, NOW())
+EOF;
+
+try
+{
+    $sth = $dbh->prepare($sql);
+    $sth->execute(array(':stationId'  => $stationId,
+                        ':sensorType' => $sensorType,
+                        ':value'      => $value,
+                        ':sequence'   => $sequence
+    ));
+}
+catch (Exception $e)
+{
+    // TODO: Check exception and echo better error code
+    echo '1';
+    exit();
+}
+
+echo '0';
